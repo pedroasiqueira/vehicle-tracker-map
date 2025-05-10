04a78679faacf587d08c7867e54cbae43665ee7b
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2025-05-10 17:57:10 -0300

    inicializando projeto
    instalando dependencias

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-red-600">
+      Tailwind funcionando! 🚀
+    </div>
+  );
+}
+
+export default App;
